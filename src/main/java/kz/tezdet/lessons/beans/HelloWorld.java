1a63ea8d09f4d7ff6f4664085258829e8a3ceedc
daulet <daucloudlab@gmail.com>
2015-12-09 13:13:12 +0600

    Spring Core demo app

---
+++ src/main/java/kz/tezdet/lessons/beans/HelloWorld.java
@@ -0,0 +1,17 @@
+package kz.tezdet.lessons.beans;
+
+public class HelloWorld {
+    private String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println("Spring : Hello " + name);
+    }
+}
